146ee277d9d41f04a914865fd48f1d4290593351
Josip Kuntic <jokuntic@vsite.hr>
2016-10-13 21:23:45 +0200

    Defaultni namespace

---
+++ Postavke_imenika/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Vsite.CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MojaKlasa1 mk1 = new MojaKlasa1();
+            MojaKlasa2 mk2 = new MojaKlasa2();
+            Console.WriteLine("Bok");
+        }
+    }
+}
