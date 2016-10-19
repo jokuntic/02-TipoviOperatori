3f06b5fc376b8cb031a151edcf80ba01995b705d
Josip Kuntic <jokuntic@vsite.hr>
2016-10-20 00:10:40 +0200

    Svojstva brojčanih tipova.

--- UgrađeniBrojčaniTipovi/Program.cs
+++ UgrađeniBrojčaniTipovi/Program.cs
@@ -7,6 +7,16 @@ namespace CSharp.UgrađeniBrojčaniTipovi
         static void Main(string[] args)
         {
             Console.WriteLine(int.MaxValue);
+            Console.WriteLine(int.MinValue);
+            int x = int.MaxValue;
+            Console.WriteLine(x+1);
+            int y = int.MinValue;
+            Console.WriteLine(y - 1);
+            Console.WriteLine("{0} - {1}", double.MinValue, double.MaxValue);
+            Console.WriteLine(3.5 / 0);
+            Console.WriteLine(-3.5 / 0);
+            double d = 0;
+            Console.WriteLine(d / 0);
 
 
 
