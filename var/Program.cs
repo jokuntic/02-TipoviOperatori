9342fc7477ffc6f7f9b31e69351e9bf1e1061c8a
Josip Kuntic <jokuntic@vsite.hr>
2016-10-20 10:48:26 +0200

    Deklaracija ključnom riječi var.

--- var/Program.cs
+++ var/Program.cs
@@ -5,13 +5,21 @@ namespace CSharp.TipoviOperatori
     {
         public int a;
         public double b;
+        //u strukturi nije moguće inicijalizirati članove
     }
 
     class Program
     {
         static void Main(string[] args)
         {
+            var i0 = 5;
+            var d0 = 3.5;
+            // i0 = d0; //nije moguća implicitna konverzija double u int
 
+            int i1 = 5;
+            double d1 = 3.5;
+
+            var s = new S();
         }
     }
 }
